505167870639a49816805f3078bced431783771e
hsylin <hylin.cs@gmail.com>
2026-04-28 01:31:45 +0200

    chore(hw2): add build and run scripts

---
+++ HW2/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set -x
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y g++ nlohmann-json3-dev
+
+mkdir -p build-release
+
+mapfile -d '' SOURCES < <(
+    find src -name '*.cpp' \
+        ! -path 'src/backend/loop_scheduling.cpp' \
+        -print0 | sort -z
+)
+
+g++ -std=c++20 -O2 -Wall -Wextra \
+    -I. \
+    -Isrc \
+    -Isrc/common \
+    -Isrc/frontend \
+    -Isrc/middleend \
+    -Isrc/backend \
+    -Isrc/backend/output \
+    -Isrc/backend/looppip \
+    "${SOURCES[@]}" \
+    -o build-release/hw2
---
+++ HW2/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+./build-release/hw2 "$1" "$2" "$3"
